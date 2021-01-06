2efbe33bdd9f3d8749f8a24378b4f47e067daf48
Adam Piggott <4228567+ProactiveServices@users.noreply.github.com>
2021-01-06 23:57:31 +0000

    Remove third-party DNS lookups.

--- src/models/constants.go
+++ src/models/constants.go
@@ -19,34 +19,14 @@ var (
 
 func init() {
 	var err error
-	DEFAULT_RELAY, err = lookupIP(DEFAULT_RELAY)
 	if err == nil {
 		DEFAULT_RELAY += ":" + DEFAULT_PORT
 	} else {
 		DEFAULT_RELAY = ""
 	}
-	DEFAULT_RELAY6, err = lookupIP(DEFAULT_RELAY6)
 	if err == nil {
 		DEFAULT_RELAY6 = "[" + DEFAULT_RELAY6 + "]:" + DEFAULT_PORT
 	} else {
 		DEFAULT_RELAY6 = ""
 	}
-}
-
-func lookupIP(address string) (ipaddress string, err error) {
-	r := &net.Resolver{
-		PreferGo: true,
-		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-			d := net.Dialer{
-				Timeout: time.Millisecond * time.Duration(10000),
-			}
-			return d.DialContext(ctx, "udp", "1.1.1.1:53")
-		},
-	}
-	ip, err := r.LookupHost(context.Background(), address)
-	if err != nil {
-		return
-	}
-	ipaddress = ip[0]
-	return
 }
